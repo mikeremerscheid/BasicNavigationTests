2d2799636b52da030164bf36b5b697a5bf5fd50d
mikeremerscheid <mikeremerscheid@gmail.com>
2020-03-11 23:54:26 -0400

    Working on TitleVerification2.

---
+++ src/test/java/com/cbt/utilities/TitleVerification2.java
@@ -0,0 +1,25 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TitleVerification2 {
+
+    public static void main(String[] args) {
+
+        List<String> urls = Arrays.asList("https://luluandgeorgia.com",
+                "https://wayfair.com/", "https://walmart.com", "https://westelm.com/");
+
+        WebDriver driver = BrowserFactory.GetDriver("firefox");
+        for (String each: urls){
+            driver.get(each);
+            String siteURL = driver.getCurrentUrl().replace("https://","").replace(".com","");
+            String siteTitle = driver.getTitle().trim();
+            System.out.println(siteTitle.contains(siteURL));
+        }
+
+        driver.quit();
+    }
+}
